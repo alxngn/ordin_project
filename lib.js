fd7f52a9716ff15d9041ca9dac56d5a93c2b491b
alxngn <alexviean@gmail.com>
2021-06-11 15:30:41 -0400

    Update learning progress

---
+++ lib.js
@@ -0,0 +1,41 @@
+// Title: My own functions created for future reference
+//// Note: usages and explanation
+
+// Get a random item in an array
+//// (Math.floor(Math.random() * x) gives a random number between 0 and x
+function randomItem(arrayName) {
+    return arrayName[Math.floor(Math.random() * arrayName.length)];
+}
+// Get a random number between 2 numbers. [min->max) from inclusive upto exclusive
+//// Only handles bigger second number than first number
+function random(firstNum, secNum) {
+let num;
+ if (firstNum < secNum) {
+  num = Math.floor(Math.random() * secNum);
+   while (num < firstNum) {
+   num = Math.floor(Math.random() * secNum);
+   }
+ } else if (firstNum == secNum) {
+ num = secNum;
+ } else {
+  num = null;
+  alert('Second number should be greater than first number!');
+ }
+return num;
+}
+// Get and combine all input values into a string separated by comma
+//// Use split(',') to convert to an array/object
+let inputs = document.getElementsByClassName( 'usrInput' ),
+    names  = [].map.call(inputs, function( input ) {
+        return input.value;
+    }).join( ',' );
+// Check if variable is an integer
+//// Return true or false
+function isInt(value) {
+  return (
+    !isNaN(value) &&
+    parseInt(Number(value)) == value &&
+    !isNaN(parseInt(value, 10))
+  );
+}
+//
